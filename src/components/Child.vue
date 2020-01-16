d2eb0490bc7bf4075704833171e7bbdcdf0743f1
Zxq-zn <Zxq_zn@163.com>
2020-01-16 18:59:44 +0800

    vue代码记录

---
+++ src/components/Child.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>这里是子组件</p>
+        <p>{{msg}}</p>
+        <form>
+            <input type="text" v-model="childMsg">
+            <button @click.prevent="getData">点击子组件</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            childMsg:''
+        }
+    },
+    props:['msg'],
+    methods:{
+        getData(){
+            this.$emit('changeData',this.childMsg)
+        }
+    }
+}
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>这里是父组件</p>
+        <Child :msg="message" @changeData="getChildData"></Child>
+    </div>
+</template>
+<script>
+import Child from '@/components/Child.vue'
+
+export default {
+    data(){
+        return {
+            message:'123'
+        }
+    },
+    components:{
+        Child
+    },
+    methods:{
+        getChildData(data){
+            console.log(data);
+        }
+    }
+}
+</script>
